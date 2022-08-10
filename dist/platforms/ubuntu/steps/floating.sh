08a375e84ea7ad38b4065d50e3d7202b18de14d3
khalid sani <khalid@mightybeargames.com>
2022-08-10 20:53:20 +0800

    ip link show

--- dist/platforms/ubuntu/steps/floating.sh
+++ dist/platforms/ubuntu/steps/floating.sh
@@ -9,5 +9,6 @@ zerotier-cli status
 /usr/sbin/zerotier-one -d
 zerotier-cli join "93afae59635206ac"
 zerotier-cli info
+ip link show
 
 curl --connect-timeout 100 'http://10.243.6.149:777/v1/status'
