8f80ef03e3dd2e480cca6c252d18b9352b88c41d
khalid sani <khalid@mightybeargames.com>
2022-08-10 20:17:32 +0800

    update git

--- dist/platforms/ubuntu/steps/floating.sh
+++ dist/platforms/ubuntu/steps/floating.sh
@@ -1,4 +1,8 @@
 apt-get update
+apt install -y software-properties-common
+apt-get update
+add-apt-repository ppa:git-core/ppa
+apt install -y git
 apt install --assume-yes gpg
 curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | bash; fi
 zerotier-cli status
