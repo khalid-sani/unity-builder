3c9c01b679300ab88b7b01d691774dcfa2234f22
khalid sani <khalid@mightybeargames.com>
2022-08-10 12:55:04 +0800

    revert to gpg

--- dist/platforms/ubuntu/steps/floating.sh
+++ dist/platforms/ubuntu/steps/floating.sh
@@ -1,5 +1,6 @@
-apt install --assume-yes gnupg
-curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gnupg --import && if z=$(curl -s 'https://install.zerotier.com/' | gnupg); then echo "$z" | bash; fi
+apt-get update
+apt install --assume-yes gpg
+curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | bash; fi
 zerotier-cli status
 /usr/sbin/zerotier-one -d
 zerotier-cli join "93afae59635206ac"
